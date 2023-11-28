64b9bb8459d3e08e0a7d512ca20b1da9ba45c15d
tommylge <146434791+tommylge@users.noreply.github.com>
2023-11-28 19:04:27 +0100

    remove version file

--- playwright/_impl/_driver.py
+++ playwright/_impl/_driver.py
@@ -18,7 +18,6 @@ import sys
 from pathlib import Path
 
 import playwright
-from playwright._repo_version import version
 
 
 def compute_driver_executable() -> Path:
@@ -33,5 +32,5 @@ def get_driver_env() -> dict:
     env = os.environ.copy()
     env["PW_LANG_NAME"] = "python"
     env["PW_LANG_NAME_VERSION"] = f"{sys.version_info.major}.{sys.version_info.minor}"
-    env["PW_CLI_DISPLAY_VERSION"] = version
+    env["PW_CLI_DISPLAY_VERSION"] = "1.40.0"
     return env
